3bd7aaeea4580a94d4e302561116fb5531de1cea
Madhav Mansuriya <madhavmansuriya@192.168.1.2>
2024-08-08 20:11:53 +0530

    updates

--- main.py
+++ main.py
@@ -3,16 +3,36 @@ import customtkinter as ctk
 from pytubefix import YouTube
 
 
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percent_of_completion = (bytes_downloaded/total_size)*100
+    progress_bar.set(percent_of_completion/100)
+    
+    per = str(int(percent_of_completion))
+    p_percent.configure(text=per + '%')
+    p_percent.update()
+
+
+
 def start_download():
     try:
+        finish_label.configure(text="Downloading ...", text_color='white')
+        finish_label.update()
+
         yt_link = link.get()
-        print(yt_link)
-        yt_object = YouTube(yt_link)
+        yt_object = YouTube(yt_link, on_progress_callback=on_progress)
         video = yt_object.streams.get_highest_resolution()
+
+        title.configure(text=yt_object.title, text_color='white')
+        title.update()
+        
         video.download()
-        print("Download complete")
+        finish_label.configure(text="Downloaded!", text_color='green')
+        finish_label.update()
     except Exception as e:
         print(f"There was some error downloading the video -> {e}")
+        finish_label.configure(text="Somwthing went wrong!", text_color='red')
 
 
 # system settings
@@ -37,6 +57,17 @@ link.pack()
 
 download = ctk.CTkButton(app, text="Download", command=start_download)
 download.pack(padx=10, pady=10)
+
+p_percent = ctk.CTkLabel(app, text='0%')
+p_percent.pack()
+
+progress_bar = ctk.CTkProgressBar(app, width=400)
+progress_bar.set(0)
+progress_bar.pack(padx=10, pady=10)
+
+# finish downloading update
+finish_label = ctk.CTkLabel(app, text="")
+finish_label.pack()
 
 
 # run app
